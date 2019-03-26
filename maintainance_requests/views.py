053880142c59d501e31cc8fa9dd08a4978d18d17
james kisuule <kisuulejames1@gmail.com>
2019-03-26 17:36:50 +0300

    Modify views

--- maintainance_requests/views.py
+++ maintainance_requests/views.py
@@ -6,6 +6,10 @@ from rest_framework.filters import SearchFilter
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
+from rest_framework.exceptions import APIException
+from rest_framework import status
+from django.http import JsonResponse
+import logging
 
 class MaintainanceRequestViewSet(ModelViewSet):
     """API Endpoint for viewing and editting user details"""
@@ -21,11 +25,18 @@ class MaintainanceRequestViewSet(ModelViewSet):
     def perform_update(self, serializer):
         if self.request.user.is_superuser and "status" in self.request.data:
             serializer.save()
-        elif "status" in self.request.data:
-            return Response({'Message': 'You have successfully register'})
-        elif not self.request.user.is_superuser:
-            serializer.save(author=self.request.user)
-        
+        elif "status" in self.request.data and not self.request.user.is_superuser:
+            return Response("Unauthorized", status.HTTP_401_UNAUTHORIZED)
+
+    def perform_update(self, serializer):
+        logger = logging.getLogger(__name__)
+        logger.error("The message")
+        # if self.request.user.is_superuser:
+        #     if "status" in self.request.data:
+        #         serializer.save()
+        #     return JsonResponse({"Missing some fields":"jfjjfjfkjfjdfkj"})
+        # return JsonResponse({"You are not authorized": "jjjjjjdjdjdjdjdjd"})
+
     def list(self, request):
         if request.user.is_superuser:
             queryset = MaintainanceRequest.objects.all()
